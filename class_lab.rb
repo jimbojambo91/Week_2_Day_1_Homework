3bc7d672fe39544cf09cde5baa2d9b7c4d214bb1
James Duncan <james.a.duncan91@gmail.com>
2017-03-13 15:58:02 +0000

    Finished section B

--- class_lab.rb
+++ class_lab.rb
@@ -29,6 +29,52 @@ class CodeclanStudent
   def favourite_language(language)
     return "I love #{language}!"
   end
-
-
 end
+
+class SportsTeam
+  attr_accessor :team_name, :players, :coach, :points
+
+  def initialize(team_name, players, coach)
+    @team_name = team_name
+    @players = players
+    @coach = coach
+    @points = 0
+    
+  end
+
+  def add_new_player(player)
+    @players << "Jack Hamilton"
+  end
+
+  def find_player?(player_to_find)
+    for player in @players
+      if player == player_to_find
+        return true
+      end
+    end
+    return false
+  end
+
+  def points_total(result)
+    if result == "Win"
+      @points += 3
+    end
+  end
+
+  # def update_coach_name(new_name)
+  #   @coach = new_name
+  # end
+
+  # def return_team_name
+  #   return @team_name
+  # end
+
+  # def return_players
+  #   return @players
+  # end
+
+  # def return_coach
+  #   return @coach
+  # end
+end
+
--- class_lab_specs.rb
+++ class_lab_specs.rb
@@ -35,3 +35,52 @@ class TestClassLab < MiniTest::Test
     assert_equal("I love Ruby!", @test_student.favourite_language("Ruby"))
   end
 end
+
+class TestSportsTeam < MiniTest::Test
+
+  def setup
+    @test_team = SportsTeam.new("Heart of Midlothian", ["Jamie Walker", "Sam Nicholson", "Bjorn Johnson"], "Ian Cathro")
+  end
+
+  def test_team_name
+    assert_equal("Heart of Midlothian", @test_team.team_name)
+  end
+
+  def test_player_name
+    assert_equal(["Jamie Walker", "Sam Nicholson", "Bjorn Johnson"] ,@test_team.players)
+  end
+
+  def test_coach_name
+    assert_equal("Ian Cathro", @test_team.coach)
+  end
+
+  def test_update_coach_name
+    @test_team.coach ="Craig Levein"
+    assert_equal("Craig Levein", @test_team.coach)
+  end
+
+  def test_add_new_player
+    @test_team.add_new_player("Jack Hamilton")
+    assert_equal(["Jamie Walker", "Sam Nicholson", "Bjorn Johnson", "Jack Hamilton"], @test_team.players)
+  end
+
+  def test_find_player_true?
+    result = @test_team.find_player?("Jamie Walker")
+    assert_equal(true, result)
+  end
+
+  def test_find_player_false?
+    result = @test_team.find_player?("Jason Cummings")
+    assert_equal(false, result)
+  end
+
+  def test_points_win
+    @test_team.points_total("Win")
+    assert_equal(3, @test_team.points)
+  end
+
+  def test_points_lose
+    @test_team.points_total("Lose")
+    assert_equal(0, @test_team.points)
+  end
+end
